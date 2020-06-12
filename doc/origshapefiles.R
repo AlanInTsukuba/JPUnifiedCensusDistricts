d96b5d6fefa73fa1aef4e0e3e3cba7a2fe537e1a
Alan Engel <ttcrossroads09@gmail.com>
2020-06-12 13:43:46 +0900

    rebuild

--- doc/origshapefiles.R
+++ doc/origshapefiles.R
@@ -7,3 +7,12 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(jpucd)
 
+## -----------------------------------------------------------------------------
+list.files(system.file("extdata",package="jpucd"))
+
+## -----------------------------------------------------------------------------
+library(jpucd)
+shppath <- system.file("extdata",package="jpucd")
+shpfile <- paste(shppath,list.files(shppath,pattern=".zip")[1],sep="/")
+kmlfile <- openkmzzip(shpfile)
+
